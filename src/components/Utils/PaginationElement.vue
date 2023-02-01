e2f1bc37b3a00596078e85db56dd7a8ff37610ce
Eugenio Topa <eugenio.topa@altecspace.it>
2023-02-01 13:59:40 +0100

    Report coverage blocked

--- src/components/Utils/PaginationElement.vue
+++ src/components/Utils/PaginationElement.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import JsonTreeView from '@/components/Utils/JsonTree/JsonTreeView.vue'
+import { reactive, ref, watch } from 'vue'
 import { isUndefined } from 'lodash'
 
 const props = defineProps<{
@@ -33,6 +34,10 @@ watch(
     emits('update:length', newLength)
   }
 )
+
+const state = reactive({
+  json: `{"string":"text","number":123,"boolean":true,"array":["A","B","C"],"object":{"prop1":"value1","nestedObject":{"prop2":"value2"}}}`,
+})
 </script>
 
 <template>
@@ -47,6 +52,7 @@ watch(
       </div>
     </span>
   </div>
+  <JsonTreeView :data="state.json" :maxDepth="3" />
 </template>
 <style scoped>
 .pagination {
