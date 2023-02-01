8a134ab6103b01012ab1ae10e1377d238201e3c2
Eugenio Topa <eugenio.topa@altecspace.it>
2023-02-01 10:02:33 +0100

    Fix

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,8 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import type { Auth } from '@/types/Auth'
 
 import App from './App.vue'
 import router from './router'
-import OidcAuthService from '@/services/OidcAuth'
-import http, { AxiosKey } from '@/services/AxiosInstance'
-
 
 import './assets/main.css'
 
-export const authService: Auth = new OidcAuthService()
-const pinia = createPinia()
-export const app = createApp(App)
-  .use(pinia)
-  .use(router)
-  .provide(AxiosKey, http)
-  .provide('authService', authService)
-  .mount('#app')
+export const app = createApp(App).use(router).mount('#app')
